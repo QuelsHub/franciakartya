e1f488b0f7ea6b7b1fb41e855f4c27eda3ade115
QuelsHub <94322679+QuelsHub@users.noreply.github.com>
2021-11-14 22:23:06 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace franciakartya
+{
+    class Program
+    {
+        
+        static void deck()
+        {
+
+            string[] Color = {"Treff","Pick","Káró","Kör"};
+            string[] Shape = {"2","3","4","5","6","7","8","9","10","J","D","K","A"};
+            int DeckSize = 52;
+
+            string[] Deck = new string[DeckSize];
+
+            int counter = 0;
+
+            for (int i = 0; i < Color.Length; i++)
+            {
+
+                for (int j = 0; j < Shape.Length; j++)
+                {
+
+                    Deck[counter] = Color[i] + " " + Shape[j];
+                    counter++;
+                }
+
+            }
+
+            counter = 0;
+
+            Console.WriteLine("A teljes pakli tartalma:\n");
+
+            foreach (var item in Deck)
+            {
+                
+                Console.Write(item + "\t");
+                counter++;
+                if (counter == 3)
+                {
+                    Console.WriteLine();
+                    counter = 0;
+                }
+            }
+
+            //string[] Hand = new string[5]; 
+            Random random = new Random();
+            HashSet<int> Rnd = new HashSet<int>();
+
+            do
+            {
+                Rnd.Add(random.Next(0, DeckSize-1));
+            } while (Rnd.Count != 5);
+
+            Console.WriteLine("\n\nA kiosztott lapjaid:\n");
+
+            foreach (var item in Rnd)
+            {
+                Console.WriteLine(Deck[item]);
+            }
+
+        }
+        
+        static void Main(string[] args)
+        {
+            deck();
+            Console.ReadKey();
+        }
+    }
+}
